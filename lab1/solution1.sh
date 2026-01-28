fe6845138478f145d9fee6a5d52cba924b401995
ngthecoder <ngoto0208@gmail.com>
2026-01-28 12:06:58 -0500

    updated to python version

--- lab1/solution1.sh
+++ lab1/solution1.sh
@@ -1,64 +1,77 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-BRIDGE_NAME="br0"
-BRIDGE_IP="192.168.100.1/24"
-TAP0_NAME="tap0"
-TAP1_NAME="tap1"
-VM1_NAME="vm1"
-VM2_NAME="vm2"
-VM1_IP="192.168.100.2/24"
-VM2_IP="192.168.100.3/24"
-GATEWAY_IP="192.168.100.1"
+import subprocess
+import time
 
-sudo ip link add name $BRIDGE_NAME type bridge
-sudo ip addr add $BRIDGE_IP dev $BRIDGE_NAME
-sudo ip link set dev $BRIDGE_NAME up
+BRIDGE_NAME = "br0"
+BRIDGE_IP = "192.168.100.1/24"
+TAP0_NAME = "tap0"
+TAP1_NAME = "tap1"
+VM1_NAME = "vm1"
+VM2_NAME = "vm2"
+VM1_IP = "192.168.100.2/24"
+VM2_IP = "192.168.100.3/24"
+GATEWAY_IP = "192.168.100.1"
 
-sudo ip tuntap add dev $TAP0_NAME mode tap
-sudo ip tuntap add dev $TAP1_NAME mode tap
-sudo ip link set dev $TAP0_NAME up
-sudo ip link set dev $TAP1_NAME up
+def run(cmd):
+    subprocess.run(cmd, shell=True, check=True)
 
-sudo ip link set dev $TAP0_NAME master $BRIDGE_NAME
-sudo ip link set dev $TAP1_NAME master $BRIDGE_NAME
+def write_file(path, content):
+    subprocess.run(f"tee {path}", shell=True, input=content.encode(), stdout=subprocess.DEVNULL)
 
-sudo lxc-create -n $VM1_NAME -t ubuntu -- --release focal
-sudo lxc-create -n $VM2_NAME -t ubuntu -- --release focal
+# Create bridge
+run(f"ip link add name {BRIDGE_NAME} type bridge")
+run(f"ip addr add {BRIDGE_IP} dev {BRIDGE_NAME}")
+run(f"ip link set dev {BRIDGE_NAME} up")
 
-cat << EOF | sudo tee /var/lib/lxc/$VM1_NAME/config
-lxc.include = /usr/share/lxc/config/ubuntu.common.conf
-lxc.rootfs.path = dir:/var/lib/lxc/$VM1_NAME/rootfs
-lxc.uts.name = $VM1_NAME
+# Create TAP devices
+run(f"ip tuntap add dev {TAP0_NAME} mode tap")
+run(f"ip tuntap add dev {TAP1_NAME} mode tap")
+run(f"ip link set dev {TAP0_NAME} up")
+run(f"ip link set dev {TAP1_NAME} up")
+
+# Add TAPs to bridge
+run(f"ip link set dev {TAP0_NAME} master {BRIDGE_NAME}")
+run(f"ip link set dev {TAP1_NAME} master {BRIDGE_NAME}")
+
+# Create LXC containers
+run(f"lxc-create -n {VM1_NAME} -t ubuntu -- --release focal")
+run(f"lxc-create -n {VM2_NAME} -t ubuntu -- --release focal")
+
+# Configure VM1
+vm1_config = f"""lxc.include = /usr/share/lxc/config/ubuntu.common.conf
+lxc.rootfs.path = dir:/var/lib/lxc/{VM1_NAME}/rootfs
+lxc.uts.name = {VM1_NAME}
 lxc.net.0.type = veth
-lxc.net.0.link = $BRIDGE_NAME
+lxc.net.0.link = {BRIDGE_NAME}
 lxc.net.0.flags = up
 lxc.net.0.name = eth0
 lxc.net.0.hwaddr = 00:16:3e:xx:xx:01
-EOF
+"""
+write_file(f"/var/lib/lxc/{VM1_NAME}/config", vm1_config)
 
-cat << EOF | sudo tee /var/lib/lxc/$VM2_NAME/config
-lxc.include = /usr/share/lxc/config/ubuntu.common.conf
-lxc.rootfs.path = dir:/var/lib/lxc/$VM2_NAME/rootfs
-lxc.uts.name = $VM2_NAME
+# Configure VM2
+vm2_config = f"""lxc.include = /usr/share/lxc/config/ubuntu.common.conf
+lxc.rootfs.path = dir:/var/lib/lxc/{VM2_NAME}/rootfs
+lxc.uts.name = {VM2_NAME}
 lxc.net.0.type = veth
-lxc.net.0.link = $BRIDGE_NAME
+lxc.net.0.link = {BRIDGE_NAME}
 lxc.net.0.flags = up
 lxc.net.0.name = eth0
 lxc.net.0.hwaddr = 00:16:3e:xx:xx:02
-EOF
+"""
+write_file(f"/var/lib/lxc/{VM2_NAME}/config", vm2_config)
 
-sudo lxc-start -n $VM1_NAME
-sudo lxc-start -n $VM2_NAME
-sleep 3
+# Start containers
+run(f"lxc-start -n {VM1_NAME}")
+run(f"lxc-start -n {VM2_NAME}")
+time.sleep(3)
 
-sudo lxc-attach -n $VM1_NAME -- bash -c "
-    ip addr add $VM1_IP dev eth0
-    ip link set dev eth0 up
-    ip route add default via $GATEWAY_IP
-"
+# Configure IPs inside containers
+run(f"lxc-attach -n {VM1_NAME} -- ip addr add {VM1_IP} dev eth0")
+run(f"lxc-attach -n {VM1_NAME} -- ip link set dev eth0 up")
+run(f"lxc-attach -n {VM1_NAME} -- ip route add default via {GATEWAY_IP}")
 
-sudo lxc-attach -n $VM2_NAME -- bash -c "
-    ip addr add $VM2_IP dev eth0
-    ip link set dev eth0 up
-    ip route add default via $GATEWAY_IP
-"
+run(f"lxc-attach -n {VM2_NAME} -- ip addr add {VM2_IP} dev eth0")
+run(f"lxc-attach -n {VM2_NAME} -- ip link set dev eth0 up")
+run(f"lxc-attach -n {VM2_NAME} -- ip route add default via {GATEWAY_IP}")
